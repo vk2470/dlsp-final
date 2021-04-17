3aa9929173eb5b24d8ff43fd5b72ae57f4f74400
vk2470 <vk2470@columbia.edu>
2021-04-17 17:49:54 -0400

    adding bashscript

---
+++ runner.sh
@@ -0,0 +1,5 @@
+python trainer.py --num_epochs 100 --percentage_labelled 0.1 --percentage_unlabelled 0.1
+
+python trainer.py --num_epochs 100 --percentage_labelled 0.1 --percentage_unlabelled 0.5
+
+python trainer.py --num_epochs 100 --percentage_labelled 0.1 --percentage_unlabelled 1.0
