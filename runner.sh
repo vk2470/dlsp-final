8f8c83db9970bb263aaae4347bd7af8bd70d3940
vk2470 <vk2470@columbia.edu>
2021-04-18 17:42:31 -0400

    100 epochs

--- runner.sh
+++ runner.sh
@@ -1,11 +1,11 @@
-python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 50 --percentage_labelled 0.5 --percentage_unlabelled 0.1 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.1 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
 
-python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 50 --percentage_labelled 0.5 --percentage_unlabelled 0.3 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.3 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
 
-python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 50 --percentage_labelled 0.5 --percentage_unlabelled 0.5 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.5 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
 
-python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 50 --percentage_labelled 0.5 --percentage_unlabelled 0.7 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.7 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
 
-python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 50 --percentage_labelled 0.5 --percentage_unlabelled 0.9 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.9 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
 
-python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 50 --percentage_labelled 0.5 --percentage_unlabelled 1.0 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 1.0 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001 --batch_size 64
