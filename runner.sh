2daf2ca1ee19c2115c53da534702072731942ae4
vk2470 <vk2470@columbia.edu>
2021-04-18 02:06:56 -0400

    adding runs

--- runner.sh
+++ runner.sh
@@ -1,5 +1,11 @@
-python trainer.py --num_epochs 100 --percentage_labelled 0.1 --percentage_unlabelled 0.1
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.1 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001
 
-python trainer.py --num_epochs 100 --percentage_labelled 0.1 --percentage_unlabelled 0.5
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.3 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001
 
-python trainer.py --num_epochs 100 --percentage_labelled 0.1 --percentage_unlabelled 1.0
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.5 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001
+
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.7 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001
+
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 0.9 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001
+
+python trainer.py --pretrainer_num_epochs 10 --finetuner_num_epochs 100 --percentage_labelled 0.5 --percentage_unlabelled 1.0 --finetuner_lr 0.001 --pretrainer_lr 0.001 --pretrainer_backbone_lr 0.0001
